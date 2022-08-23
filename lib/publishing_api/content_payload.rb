ed61f00aefabed31206e97d7f4684aacd9f738c8
Kevin Dew <kevindew@me.com>
2022-08-23 12:09:13 +0100

    Use string keys in payload hash
    This resolves an inconsistency where there was a mix of symbols and
    strings. Strings are easier to deal with as that is what comes from the
    YAML file and what is returned by the Publishing API.

--- lib/publishing_api/content_payload.rb
+++ lib/publishing_api/content_payload.rb
@@ -8,30 +8,30 @@ module PublishingApi
 
     def payload
       @payload ||= publishing_api_attributes.merge({
-        locale: publishing_api_attributes.fetch("locale", "en"),
-        schema_name: publishing_api_attributes.fetch("schema_name", "special_route"),
-        document_type: publishing_api_attributes.fetch("document_type", "special_route"),
-        publishing_app: "contentful-listener",
-        update_type: publishing_api_attributes.fetch("update_type", "major"),
-        details: details,
-        routes: build_routes,
+        "locale" => publishing_api_attributes.fetch("locale", "en"),
+        "schema_name" => publishing_api_attributes.fetch("schema_name", "special_route"),
+        "document_type" => publishing_api_attributes.fetch("document_type", "special_route"),
+        "publishing_app" => "contentful-listener",
+        "update_type" => publishing_api_attributes.fetch("update_type", "major"),
+        "details" => details,
+        "routes" => routes,
       })
-    end
-
-    def details
-      @details ||= build_details(contentful_entry)
     end
 
   private
 
     attr_reader :contentful_client, :contentful_entry, :publishing_api_attributes
 
-    def build_routes
+    def details
+      @details ||= build_details(contentful_entry)
+    end
+
+    def routes
       return publishing_api_attributes["routes"] if publishing_api_attributes["routes"]
 
       base_path = publishing_api_attributes["base_path"]
 
-      [{ path: base_path, type: "exact" }]
+      [{ "path" => base_path, "type" => "exact" }]
     end
 
     def build_details(item, entry_ids = [])
@@ -50,9 +50,9 @@ module PublishingApi
         build_details(item.resolve(contentful_client), entry_ids)
       when Contentful::Asset
         {
-          cms_entity: "asset",
-          cms_id: item.id,
-          url: item.url
+          "cms_entity" => "Asset",
+          "cms_id" => item.id,
+          "url" => item.url
         }
       else
         raise "#{item.class} is not configured to be represented as JSON for Publishing API"
@@ -61,8 +61,8 @@ module PublishingApi
 
     def entry_details(entry, entry_ids)
       base = {
-        cms_entity: "entry",
-        cms_id: entry.id
+        "cms_entity" => "Entry",
+        "cms_id" => entry.id
       }
 
       # if we've already visited an entry in this tree we're in a recursive loop
@@ -70,7 +70,7 @@ module PublishingApi
       if entry_ids.include?(entry.id)
         base
       else
-        fields = base.merge(entry.fields.except(base.keys))
+        fields = base.merge(entry.fields.transform_keys(&:to_s).except(base.keys))
         build_details(fields, entry_ids + [entry.id])
       end
     end
