79e1328fa9346916f3c5687695b943c1d674e146
Kevin Dew <kevindew@me.com>
2022-08-23 17:39:37 +0100

    Set up module for looking up Contentful clients
    This sets up a helper module that can be used to look up the access
    tokens for a Contentful client in a particular space. This has been done
    so it is easier to support multiple spaces while allowing simpler code
    to initialise a client.

---
+++ lib/contentful_client.rb
@@ -0,0 +1,39 @@
+require "contentful"
+require "yaml"
+
+module ContentfulClient
+  def self.draft_client(space_id)
+    Contentful::Client.new(access_token: draft_access_token(space_id),
+                           space: space_id,
+                           api_url: "preview.contentful.com")
+  end
+
+  def self.live_client(space_id)
+    Contentful::Client.new(access_token: live_access_token(space_id),
+                           space: space_id)
+  end
+
+  def self.draft_access_token(space_id)
+    config = access_token_config.find { |c| c["space_id"] == space_id }
+
+    raise "No access token configuration for space: #{space_id}" unless config
+
+    config["draft_access_token"]
+  end
+
+  def self.live_access_token(space_id)
+    config = access_token_config.find { |c| c["space_id"] == space_id }
+
+    raise "No access token configuration for space: #{space_id}" unless config
+
+    config["draft_access_token"]
+  end
+
+  def self.access_token_config
+    @access_token_config ||= begin
+                               contents = File.read("config/access_tokens.yaml.erb")
+                               interpolated = ERB.new(contents).result
+                               YAML.safe_load(interpolated)
+                             end
+  end
+end
